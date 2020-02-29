51f6b247cf8fe942b91da743a515ea3b3ad18926
brian-475 <brianadala15@gmail.com>
2020-02-29 10:24:40 +0300

    pushing my js hover files

---
+++ script.js
@@ -0,0 +1,53 @@
+//This is my hover js
+
+$("document").ready(function(){
+    $(" #why").mouseenter(function(){
+        $("#pizzaone").show();
+    })
+    $("#why").mouseleave(function(){
+        $("#pizzaone").hide();
+    })
+    $(" #are").mouseenter(function(){
+        $("#pizzatwo").show();
+    })
+    $("#are").mouseleave(function(){
+        $("#pizzatwo").hide();
+    })
+    $("#you").mouseenter(function(){
+        $("#pizzathree").show();
+    })
+    $("#you").mouseleave(function(){
+        $("#pizzathree").hide();
+    })
+    $("#here").mouseenter(function(){
+        $("#pizzafour").show();
+    })
+    $("#here").mouseleave(function(){
+        $("#pizzafour").hide();
+    })
+    // my hover line 2
+    $("#at").mouseenter(function(){
+        $("#pizzafive").show();
+    })
+    $("#at").mouseleave(function(){
+        $("#pizzafive").hide();
+    })
+    $("#this").mouseenter(function(){
+        $("#pizzasix").show();
+    })
+    $("#this").mouseleave(function(){
+        $("#pizzasix").hide();
+    })
+    $("#time").mouseenter(function(){
+        $("#pizzaseven").show();
+    })
+    $("#time").mouseleave(function(){
+        $("#pizzaseven").hide()()
+    })
+    $("#of").mouseenter(function(){
+        $("#pizzaeight").show();
+    })
+    $("#of"),mouseleave(function(){
+        $("#pizzaeight").hide();
+    })
+})
